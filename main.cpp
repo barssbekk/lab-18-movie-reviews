ba43b862571dba6430c4b9d1346b2ce4921b86a6
Barsbek Azamatov <bars.moderside@gmail.com>
2026-03-06 22:32:35 -0800

    Ask review

--- main.cpp
+++ main.cpp
@@ -8,12 +8,19 @@ int main() {
          << "\t[1] New nodes are added at the head of the linked list\n"
          << "\t[2] New nodes are added at the tail of the linked list\n"
          << "Choice: ";
-    int userChoice{};
+    double userChoice{};
     cin >> userChoice;
-    while (userChoice != 1 || userChoice != 2) {
+    while (userChoice < 1 || userChoice > 2) {
         cerr << "Wrong input! Choose 1 or 2. Try again: ";
         cin >> userChoice;
     }
 
+    cout << "Enter review rating 0-5: ";
+    cin >> userChoice;
+
     return 0;
+}
+
+void askReviewRating() {
+    
 }
